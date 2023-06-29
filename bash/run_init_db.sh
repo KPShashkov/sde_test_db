6736e57fcf55e51963e24b8c51032175c613ff60
KPShashkov <137519808+KPShashkov@users.noreply.github.com>
2023-06-30 00:55:43 +0300

    Update run_init_db.sh
    script added

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+docker pull postgres
+docker run --name test_pos -e POSTGRES_PASSWORD=sde_password012 -e POSTGRES_USER=test_sde -e POSTGRES_DB=demo -p 5432:5432 -d postgres
+mkdir ~/init_db
+docker cp  ~/init_db test_pos:/sql
+sleep 5
+docker exec test_pos psql -U test_sde -d demo -f /sql/init_db/demo.sql
